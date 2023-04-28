7a54c2b47f4238e15c1078a18089280c76ea3b76
krishnabharathi123 <60067388+krishnabharathi123@users.noreply.github.com>
2023-04-28 22:47:57 +0530

    Add files via upload

---
+++ workouts/Print100WithoutNumbers.java
@@ -0,0 +1,19 @@
+package week1.day5;
+
+public class Print100WithoutNumbers {
+
+	public static void main(String[] args) {
+
+		//Z-Z=0, d=100
+		
+		for (int i = 'Z'-'Z'; i <= 'd'; i++) { System.out.println(i); }
+		 
+		  
+		int i = 'd'-'A';
+		System.out.println(i); //35
+		
+		char j = 'd'-'A';
+		System.out.println(j); //#
+	}
+
+}
