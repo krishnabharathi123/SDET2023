d6ea95d558263b4675e8b0171b8f16efcff1ff40
krishnabharathi123 <60067388+krishnabharathi123@users.noreply.github.com>
2023-04-28 19:57:30 +0530

    Add files via upload

--- week1/day5/DiamondCharacter.java
+++ week1/day5/DiamondCharacter.java
@@ -2,14 +2,17 @@ package week1.day5;
 
 public class DiamondCharacter {
 
+	//Time Complexity ---> O(n)*O(m+k)==> O(n)*O(2n)==> O(n)*O(n)==> O(n^2) ---> Upper part of diamond 
+	//Upper Part+Lower Part ==> O(n^2)+O(n^2)==> O(2n^2)==> O(n^2)
+	
 	public static void main(String[] args) {
 		char ch=65;
 		int m=0;
-		int n=5;
+		int n=7;
 		//to execute the 1st part from A to F..reverse loop
-		 for(int i=n;i>=0;i--) {
+		 for(int i=n;i>=0;i--) { //O(n)
 			// to print the initial empty space Ex: i=5-->empty space=5; i=4-->empty space=4 etc..
-			for(int j=1;j<=i;j++) {
+			for(int j=1;j<=i;j++) { //O(n-1)==> (k)
 				System.out.print(" ");
 			}
 			//to print the character always after space
@@ -17,7 +20,7 @@ public class DiamondCharacter {
 			//to print the space in between 2 characters. 1st row no need space and second char
 			//from 2nd row we need space.Ex: i=4 ==> space=1, i=3 ==> space=3, i=2 ==> space=5, i=1==> space=7, i=0==>space=9
 			if(i!=n) {
-				for(int k=0;k<=m;k++) {
+				for(int k=0;k<=m;k++) { //O(m)
 					System.out.print(" ");
 				}
 				//after space printing the character
