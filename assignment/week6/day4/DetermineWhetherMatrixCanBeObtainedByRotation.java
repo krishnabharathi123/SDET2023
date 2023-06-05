8bb1003db63e1dea1b3e42e4d97d69d147abecfb
krishnabharathi123 <60067388+krishnabharathi123@users.noreply.github.com>
2023-06-05 18:23:55 +0530

    Update DetermineWhetherMatrixCanBeObtainedByRotation.java

--- assignment/week6/day4/DetermineWhetherMatrixCanBeObtainedByRotation.java
+++ assignment/week6/day4/DetermineWhetherMatrixCanBeObtainedByRotation.java
@@ -28,17 +28,60 @@ public class DetermineWhetherMatrixCanBeObtainedByRotation {
 		System.out.println(rotationMatrix(mat, target));
 	}
 	
-	private boolean rotationMatrix(int[][] mat, int[][] target) {
-		int size=mat.length; //n*n matrix
-		int i=0,j=size-1;
-		while(i<size) {
-			if(Arrays.equals(mat[i], target[j])) {
-				i++;
-				j--;
+	/*
+	 * use 2 loops 
+	 * outer loop for i and inner loop for j
+	 * create a new array named rot and add the rotated value
+	 * add rot[j][arr.length-1-j]=mat[i][j];
+	 * rotate till the rot array equals to input array --> break
+	 * return true if the rot array equals to the target array
+	 */
+	
+	private boolean rotateArr(int[][] mat, int[][] target) {
+		
+		int size=mat.length;
+		if(size==1) {
+			if(Arrays.deepEquals(mat,target)){
+				return true;
 			}
-			else return false;
-					
+			return false;
+		}
+		int[][] rot =new int[size][size];//n*n matrix
+		int[][] temp=mat;
+		while(!Arrays.deepEquals(rot, mat)) {
+			rot = new int[size][size];
+		for(int i=0;i<size;i++) {
+			
+			for(int j=0;j<size;j++) {
+				
+				rot[j][size-1-i]=temp[i][j];
+				
+				
 			}
-		return true;
+		}
+		System.out.println(Arrays.toString(rot));
+		
+		if(Arrays.deepEquals(rot,target)) return true;
+		else {
+			temp=rot;
+			
+		} 
+		}
+		return false;
 	}
+	
+	//not worring for all the solution --> this is not rotating
+	 public boolean findRotation(int[][] mat, int[][] target) {
+	        int size=mat.length; //n*n matrix
+			int i=0,j=size-1;
+			while(i<size) {
+				if(Arrays.equals(mat[i], target[j])) {
+					i++;
+					j--;
+				}
+				else return false;
+						
+				}
+			return true;
+	    }
 }
