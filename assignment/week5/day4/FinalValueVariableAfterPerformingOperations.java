7aede480576afebb24cf2838bbec18083d71d855
krishnabharathi123 <60067388+krishnabharathi123@users.noreply.github.com>
2023-06-05 18:21:10 +0530

    Add files via upload

---
+++ assignment/week5/day4/FinalValueVariableAfterPerformingOperations.java
@@ -0,0 +1,39 @@
+package assignment.week5.day4;
+
+import org.junit.Test;
+
+public class FinalValueVariableAfterPerformingOperations {
+
+	@Test
+	public void example1() {
+		String[] operations = {"--X","X++","X++"};
+		finalValue(operations);
+	}
+	
+	@Test
+	public void example2() {
+		String[] operations = {"++X","++X","X++"};
+		finalValue(operations);
+	}
+	
+	@Test
+	public void example3() {
+		String[] operations = {"X++","++X","--X","X--"};
+		finalValue(operations);
+	}
+	
+	private void finalValue(String[] operations) {
+		int x=0;
+		for(String i:operations) {
+			if(i.contains("-")) {
+				x+=-1;
+			}
+			else {
+				x+=1;
+			}
+		}
+		System.out.println(x);
+		
+	}
+
+}
